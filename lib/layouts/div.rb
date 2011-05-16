77217d08206a564543c39803151bf78d1bca7d2a
Tim Harper <timcharper@gmail.com>
2011-05-16 08:17:36 -0600

    div layout fixes

--- lib/layouts/div.rb
+++ lib/layouts/div.rb
@@ -2,10 +2,10 @@ module UberBuilder
   module Layouts
     class Div < P
       def field(field_name, field_content, label_text, options = {})
-        return li(field_content) if label_text.blank?
+        return div(field_content) if label_text.blank?
         
         div(
-          label(label_text, "#{@object_name}_#{field_name}") + "<br />\n" + field_content,
+          label(label_text, "#{@object_name}_#{field_name}") + "<br />\n".html_safe + field_content,
           options[:outer] || {}
         ) + "\n"
       end
